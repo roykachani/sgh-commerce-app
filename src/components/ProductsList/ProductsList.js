6e8aada027b55bbfe3c8f4ba81d7c90ecb9b4f9d
Roy Kachani <roy.kachani@gmail.com>
2022-01-21 03:06:14 -0300

    logica de presentacion

---
+++ src/components/ProductsList/ProductsList.js
@@ -0,0 +1,62 @@
+import { useReducer, useEffect } from 'react';
+import { FETCHING, FETCH_SUCCESS } from '../../reducers/actions/common';
+
+import { productsReducer, inicialState } from '../../reducers/products';
+import ProductCards from '../productCards/productCards';
+import { useFetch } from './../../hooks/useFetch';
+
+const ProductsList = () => {
+	const [state, dispatch] = useReducer(productsReducer, inicialState);
+
+	const [getData] = useFetch();
+
+	const getProducts = async () => {
+		try {
+			dispatch({ type: FETCHING });
+			const result = await getData(
+				`${process.env.REACT_APP_BACK_FETCH_PRODUCTS}`
+			);
+			// console.log(result.status, 'getproducts');
+
+			if (!!result & (result.status === (404 | 500))) {
+				dispatch({ type: FETCHING, payload: result });
+				//redirect
+				throw Error();
+			}
+			console.log(result, 'succes getproducts');
+			dispatch({ type: FETCH_SUCCESS, payload: result });
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	useEffect(() => {
+		getProducts();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	if (state.loading === true)
+		return (
+			<>
+				<div>cargando productos...</div>
+			</>
+		);
+	else if (!!state) {
+		console.log(state.products.data, 'datita');
+		const { products } = state;
+		return (
+			<section className="products_container">
+				<div className="text_center">
+					<h1 className="products_cards_title">Productos</h1>
+				</div>
+				<div className="products_cards_box">
+					{products.data.map((p) => (
+						<ProductCards key={p._id} {...p} />
+					))}
+				</div>
+			</section>
+		);
+	}
+};
+
+export default ProductsList;
