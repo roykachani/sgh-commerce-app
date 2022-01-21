d1ef2da7813f27f7587870d1ec15ed25b26a12da
Roy Kachani <roy.kachani@gmail.com>
2022-01-21 03:05:19 -0300

    componente card

--- src/components/productCards/productCards.js
+++ src/components/productCards/productCards.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ProductCards = ({ products }) => {
+const ProductCards = (products) => {
+	const p = products;
+
 	return (
-		<div className="cards_box">
-			{products.map((p) => {
-				<div className="card_item">
-					<div className="card_img">{p.photo}</div>
-					<div className="card_body">
-						<div className="card_title">{p.title}</div>
-						<div className="card_price">{p.price}</div>
-						<div className="card_btns"></div>
+		<div className="card_item">
+			<div className="card">
+				<div className="card_img">
+					<img src={p.photos[0]} alt={p.title} className="picture" />
+				</div>
+				<div className="card_body">
+					<div className="card_title">{p.title}</div>
+					<div className="card_price">{p.price}</div>
+					<div className="card_btns">
+						<Link to={`/producto/${p._id}`}>
+							<button className="btn_secondary btn_card btn_card_secondary">
+								ver producto
+							</button>
+						</Link>
+						<button className="btn_primary btn_card btn_card_primary">
+							agregar
+						</button>
 					</div>
-				</div>;
-			})}
+				</div>
+			</div>
 		</div>
 	);
 };
 
 export default ProductCards;
+
+//para product
+//	{p.photos.map((photo, index) => (
+// <img src={photo} alt={p.title} key={index} />
+// ))}
