08a256f77d1fb118736a8f427d1b10a4ca12222b
Roy Kachani <roy.kachani@gmail.com>
2022-01-21 03:03:00 -0300

    view de presentacion

--- src/views/Products/Products.js
+++ src/views/Products/Products.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import ProductsList from '../../components/ProductsList/ProductsList';
+
+import './styles.css';
 
 const Products = () => {
-	return <div>Products</div>;
+	return (
+		<>
+			<main className="main_container">
+				<Header />
+				<section className="products_main_container">
+					<ProductsList />
+				</section>
+			</main>
+			{/* <div>Footer</div> */}
+		</>
+	);
 };
 
 export default Products;
