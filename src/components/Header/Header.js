d04583b45e6f27e58a29e4eac0c6fbc6d3966349
Roy Kachani <roy.kachani@gmail.com>
2022-01-31 20:44:36 -0300

    nuevo icono user y estilos

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,8 @@ import BagIcon from '../Icons/BagIcon';
 
 import LogoW from '../../assets/logo_w.png';
 import './styles.css';
+import { Link } from 'react-router-dom';
+import UserIcon from '../Icons/UserIcon';
 
 export default function Header() {
 	const [scroll, setScroll] = useState(false);
@@ -16,21 +18,32 @@ export default function Header() {
 		<div className={scroll ? 'header_container active' : 'header_container'}>
 			<div className="nav_items">
 				<div className="nav_box1">
-					<div className="nav_logo">
-						<img src={LogoW} alt="CAVERN | Logo empresarial" />
-					</div>
+					<Link to="/">
+						<div className="nav_logo">
+							<img src={LogoW} alt="CAVERN | Logo empresarial" />
+						</div>
+					</Link>
 					<div className="nav_menu">
-						<div className="nav_status">HOME</div>
-						<div className="nav_status">PRODUCTOS</div>
-						<div className="nav_status">CONTACTO</div>
+						<Link to="/home">
+							<div className="nav_status">HOME</div>
+						</Link>
+						<Link to="/products">
+							<div className="nav_status">PRODUCTOS</div>
+						</Link>
+						<Link to="/contact">
+							<div className="nav_status">CONTACTO</div>
+						</Link>
 					</div>
 				</div>
 				<div className="nav_box2">
 					<div className="box_items">
+						<div className="nav_user">
+							<UserIcon />
+						</div>
 						<div className="nav_cart">
 							<BagIcon />
 						</div>
-						<div className="btn_secondary nav_login">Registar/Ingresar</div>
+						{/* <div className="btn_secondary nav_login">Registar/Ingresar</div> */}
 					</div>
 				</div>
 			</div>
