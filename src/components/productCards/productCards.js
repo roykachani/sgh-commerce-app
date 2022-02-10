73cd44f77251017a1a553611ac98d27733d1d31e
Roy Kachani <roy.kachani@gmail.com>
2022-02-10 12:01:33 -0300

    refactor

--- src/components/productCards/productCards.js
+++ src/components/productCards/productCards.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { currencyFormat } from '../../utils/currencyConvert';
+import './styles.css';
 
 const ProductCards = (products) => {
 	const p = products;
@@ -16,6 +17,74 @@ const ProductCards = (products) => {
 		return currencyFormat(p.price);
 	};
 
+	if (p?.condition === 'offer') {
+		return (
+			<div className={`card_item ${state}`}>
+				<div className="card">
+					<div className="card_img">
+						<Link to={`/product/${p._id}`} className="link_card">
+							<img src={p.photos[0]} alt={p.title} className="picture" />
+						</Link>
+					</div>
+					<div className="card_body">
+						<div className="card_text_box">
+							<Link
+								to={`/product/${p._id}`}
+								className="link_card link_card_title"
+							>
+								<p className="card_text card_title">{p.title.toUpperCase()}</p>
+							</Link>
+							<p className="card_text card_price">{newPrice()}</p>
+						</div>
+						<div className="card_btns">
+							<Link to={`/product/${p._id}`}>
+								<button className="btn_secondary btn_card btn_card_secondary">
+									ver producto
+								</button>
+							</Link>
+							<button className="btn_primary btn_card btn_card_primary">
+								agregar +
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+	if (p?.condition === 'new') {
+		return (
+			<div className={`card_item ${state}`}>
+				<div className="card">
+					<div className="card_img">
+						<Link to={`/product/${p._id}`} className="link_card">
+							<img src={p.photos[0]} alt={p.title} className="picture" />
+						</Link>
+					</div>
+					<div className="card_body">
+						<div className="card_text_box">
+							<Link
+								to={`/product/${p._id}`}
+								className="link_card link_card_title"
+							>
+								<p className="card_text card_title">{p.title.toUpperCase()}</p>
+							</Link>
+							<p className="card_text card_price">{newPrice()}</p>
+						</div>
+						<div className="card_btns">
+							<Link to={`/product/${p._id}`}>
+								<button className="btn_secondary btn_card btn_card_secondary">
+									ver producto
+								</button>
+							</Link>
+							<button className="btn_primary btn_card btn_card_primary">
+								agregar +
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 	return (
 		<div className={`card_item ${state}`}>
 			<div className="card">
