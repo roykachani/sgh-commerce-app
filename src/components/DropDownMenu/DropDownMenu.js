ceb7d46323cb81a3c822929b02c7e47b9feaf42e
Roy Kachani <roy.kachani@gmail.com>
2022-02-10 12:05:15 -0300

    nuevos items

--- src/components/DropDownMenu/DropDownMenu.js
+++ src/components/DropDownMenu/DropDownMenu.js
@@ -1,7 +1,9 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
 import { AuthContext } from '../../context/Auth';
 import { ModalContext } from '../../context/Modal';
+import Logout from '../Logout/Logout';
 
 import './styles.css';
 
@@ -15,20 +17,22 @@ export const DropDownMenu = () => {
 
 	const DropDownItem = ({ children, href }) => {
 		return (
-			<Link to={`/${href}`}>
-				<a href={`/${href}`} className="menu_item">
-					{children}
-				</a>
+			<Link to={`/${href}`} className="menu_item">
+				{children}
 			</Link>
 		);
 	};
 
-	if (!!userState.userData) {
+	if (userState.userData?.JWT) {
 		return (
 			<>
 				<div className="bg_menu" onClick={handlerMenu}>
 					<div className={!!state.modal ? 'dropdown user_active' : 'dropdown'}>
-						<DropDownItem href={'user'}>Perfil</DropDownItem>
+						<DropDownItem href={'MyProfile'}>Perfil</DropDownItem>
+						<DropDownItem href={'MyOrders'}>Mis compras</DropDownItem>
+						<DropDownItem href={''}>
+							<Logout />
+						</DropDownItem>
 					</div>
 				</div>
 			</>
@@ -38,8 +42,8 @@ export const DropDownMenu = () => {
 	return (
 		<div className="bg_menu" onClick={handlerMenu}>
 			<div className={!!state.modal ? 'dropdown user_active' : 'dropdown'}>
-				<DropDownItem href={'signin'}>Iniciar sesion</DropDownItem>
-				<DropDownItem href={'signup'}>Registrarse</DropDownItem>
+				<DropDownItem href={'signIn'}>Iniciar sesion</DropDownItem>
+				<DropDownItem href={'signUp'}>Registrarse</DropDownItem>
 			</div>
 		</div>
 	);
