ce14afb45aee20cde804611781ca8b00373b2383
Roy Kachani <roy.kachani@gmail.com>
2022-03-01 00:21:27 -0300

    modalcontext implementacion

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+
+import { ModalContext } from '../../context/Modal';
 import Footer from '../../components/Footer/Footer';
 import ProductsSale from '../../components/ProductSale/ProductSale';
 import Welcome from '../../components/Welcome/welcome';
@@ -5,18 +8,22 @@ import Welcome from '../../components/Welcome/welcome';
 import './styles.css';
 
 const Home = () => {
+	const { state, handlerUser, handlerCart } = useContext(ModalContext);
+	const handlerMenu = () => {
+		if (!!state.modalCart) handlerCart();
+		if (!!state.modalUser) handlerUser();
+	};
+
 	return (
 		<>
-			<main className="main_container">
+			<main onClick={handlerMenu} className="main_container">
 				<section className="welcome_container">
 					<Welcome />
 				</section>
 				<section className="productsSale_container">
 					<ProductsSale />
 				</section>
-				{/* <section className="carrousel_container">
-					carrousel metodos de envio
-				</section> */}
+
 				<Footer />
 			</main>
 		</>
