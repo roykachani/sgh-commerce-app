3796482b5684e00e8cc7e55bb6c3c245e634fba1
Roy Kachani <roy.kachani@gmail.com>
2022-03-01 00:14:17 -0300

    implementacion de modalscontext

--- src/views/Products/Products.js
+++ src/views/Products/Products.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { ModalContext } from '../../context/Modal';
 import ProductsList from '../../components/ProductsList/ProductsList';
 import Footer from '../../components/Footer/Footer';
 
 import './styles.css';
 
 const Products = () => {
+	const { state, handlerUser, handlerCart } = useContext(ModalContext);
+	const handlerMenu = () => {
+		if (!!state.modalCart) handlerCart();
+		if (!!state.modalUser) handlerUser();
+	};
+
 	return (
 		<>
-			<main className="main_container">
+			<main onClick={handlerMenu} className="main_container">
 				<section className="products_main_container">
 					<ProductsList />
 				</section>
