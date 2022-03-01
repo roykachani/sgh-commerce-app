63e60ad1ba16913596fe0f227a605704d56de097
Roy Kachani <roy.kachani@gmail.com>
2022-03-01 00:18:43 -0300

    nuevas rutas

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -12,6 +12,10 @@ import Signin from '../views/SignIn/Signin';
 import Signup from '../views/Signup/Signup';
 import Header from '../components/Header/Header';
 import AccounteCreate from '../views/AccounteCreate/AccounteCreate';
+import Category from '../views/Products/Category';
+import PrivateRoute from './PrivateRoute';
+import Checkout from '../views/Checkout/Checkout';
+import Loader from '../components/Loader/Loader';
 
 const AppRouter = () => {
 	return (
@@ -20,15 +24,23 @@ const AppRouter = () => {
 			<Switch>
 				<Route path="/" exact component={Home} />
 				<Route path="/home" exact component={Home} />
-				<Route path="/signin" component={Signin} />
-				<Route path="/signup" component={Signup} />
-				<Route path="/accounteCreate" component={AccounteCreate} />
+				<Route path="/signin" exact component={Signin} />
+				<Route path="/signup" exact component={Signup} />
+				<Route path="/accounteCreate" exact component={AccounteCreate} />
 				<Route
 					path="/accounteCreate/:verificationCode"
+					exact
 					component={AccounteCreate} //desarrollar
 				/>
-				<Route path="/products" component={Products} />
-				<Route path="/product/:id" component={Product} />
+				<Route path="/products" exact component={Products} />
+				<Route
+					path="/products/categories/:category"
+					exact
+					component={Category}
+				/>
+				<Route path="/products/:id/:title" exact component={Product} />
+				<PrivateRoute exact path="/cvn/checkout/cart" component={Checkout} />
+				<PrivateRoute exact path="/cvn/checkout/onepage" component={Checkout} />
 				<Redirect to="/" />
 			</Switch>
 		</Router>
