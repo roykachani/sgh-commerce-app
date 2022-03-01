07630332baa08f64d77a86b913cea63dca62e77d
Roy Kachani <roy.kachani@gmail.com>
2022-03-01 00:20:33 -0300

    componente

--- src/views/AccounteCreate/AccounteCreate.js
+++ src/views/AccounteCreate/AccounteCreate.js
@@ -1,10 +1,19 @@
+import { useContext } from 'react';
+
+import { ModalContext } from '../../context/Modal';
 import Footer from '../../components/Footer/Footer';
 import VerifyAccount from '../../components/VerifyAccount/VerifyAccount';
 
 const AccounteCreate = () => {
+	const { state, handlerUser, handlerCart } = useContext(ModalContext);
+	const handlerMenu = () => {
+		if (!!state.modalCart) handlerCart();
+		if (!!state.modalUser) handlerUser();
+	};
+
 	return (
 		<>
-			<main className="main_container">
+			<main onClick={handlerMenu} className="main_container">
 				<section className="form_main_container">
 					<VerifyAccount />
 				</section>
